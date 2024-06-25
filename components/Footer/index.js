46c1be63c999591789f1d4acc2af0870c328abaf
marchaye <marchaye@cisco.com>
2024-06-25 15:04:37 -0400

    Pushing initial code

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -10,13 +10,6 @@ const Footer = ({}) => {
         <div>
           <h1 className="text-2xl text-bold">Contact.</h1>
           <div className="mt-10">
-            <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
-              LET&apos;S WORK
-            </h1>
-            <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
-              TOGETHER
-            </h1>
-            <Button type="primary">Schedule a call</Button>
             <div className="mt-10">
               <Socials />
             </div>
@@ -24,7 +17,7 @@ const Footer = ({}) => {
         </div>
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
-        Made With ❤ by{" "}
+        Website template provided by{" "}
         <Link href="http://www.chetanverma.com">
           <a className="underline underline-offset-1">Chetan Verma</a>
         </Link>
